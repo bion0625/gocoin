6ca58ba73d690abfae97555566e31c4bb84424ab
이의정 <stxtory@gmail.com>
2023-08-18 21:43:40 +0900

    structs with pointers

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type person struct {
-	name string
-	age  int
-}
-
-func (instance person) SayHello(){
-	fmt.Printf("Hello my name is %s and I'm %d\n", instance.name, instance.age)
-}
-
-func (p person) SayKoranAge(){
-	fmt.Printf("my korean age is %d\n", p.age+2)
-}
+	"github.com/gocoin/person"
+)
 
 func main() {
-	uj := person{
-		name: "uj",
-		age:  32,
-	}
-	uj.SayHello()
-	uj.SayKoranAge()
+	uj := person.Person{}
+	uj.SetDetails("uj",32)
+	fmt.Println("main's uj", uj)
+	fmt.Println(uj.Name())
 }
