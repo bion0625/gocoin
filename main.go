dec3f396128a0e3aa812ddc0be807f02bcae5022
이의정 <stxtory@gmail.com>
2023-08-18 14:42:29 +0900

    letters for strings

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
-func main() {
+import "fmt"
+
+func test(name string){
+	for _, letters := range name{
+		fmt.Println(string(letters))
+	}
 }
+
+func main() {
+	name := "is my name"
+	test(name)
+}
+
