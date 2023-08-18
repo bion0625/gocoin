cdeaac10c8eb76f7761fb69a2fb732beebb0d2f3
이의정 <stxtory@gmail.com>
2023-08-18 21:10:12 +0900

    struct

--- main.go
+++ main.go
@@ -2,9 +2,24 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
+func (instance person) SayHello(){
+	fmt.Printf("Hello my name is %s and I'm %d\n", instance.name, instance.age)
+}
+
+func (p person) SayKoranAge(){
+	fmt.Printf("my korean age is %d\n", p.age+2)
+}
+
 func main() {
-	a := 2
-	b := &a
-	a = 50
-	fmt.Println(*b)
+	uj := person{
+		name: "uj",
+		age:  32,
+	}
+	uj.SayHello()
+	uj.SayKoranAge()
 }
