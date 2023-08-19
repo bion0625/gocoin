f0154fce960b70bf940290ee88726603dc221ce7
이의정 <stxtory@gmail.com>
2023-08-19 21:45:47 +0900

    blockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,20 +1,57 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
 type block struct {
-	data     string
-	hash     string
-	prevHash string
+	Data     string
+	Hash     string
+	PrevHash string
 }
 
 type blockChain struct {
-	blocks []block
+	blocks []*block
 }
 
 var b *blockChain
+var once sync.Once
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockChain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockChain().blocks[totalBlocks-1].Hash
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockChain) AddBlock(data string){
+	b.blocks = append(b.blocks, createBlock(data))
+}
 
 func GetBlockChain() *blockChain {
 	if b == nil {
-		b = &blockChain{}
+		once.Do(func() {
+			b = &blockChain{}
+			b.AddBlock("Genesis Block")
+		})
 	}
 	return b
+}
+
+func (b *blockChain) AllBlocks() []*block {
+	return b.blocks
 }
