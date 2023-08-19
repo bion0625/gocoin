94a92729d2fe4795c38edf83be63a463a8143f0f
이의정 <stxtory@gmail.com>
2023-08-19 13:03:35 +0900

    refactoring singleton

---
+++ blockchain/blockchain.go
@@ -0,0 +1,20 @@
+package blockchain
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
+type blockChain struct {
+	blocks []block
+}
+
+var b *blockChain
+
+func GetBlockChain() *blockChain {
+	if b == nil {
+		b = &blockChain{}
+	}
+	return b
+}
--- main.go
+++ main.go
@@ -1,46 +1,12 @@
 package main
 
 import (
-	"crypto/sha256"
 	"fmt"
+
+	"github.com/gocoin/blockchain"
 )
 
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
-
-type blockChain struct {
-	blocks []block
-}
-
-func (b blockChain) getLastHash() string {
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-	return ""
-}
-
-func (b *blockChain) addBlock(data string){
-	newBlock := block{data, "", b.getLastHash()}
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-	newBlock.hash = fmt.Sprintf("%x", hash)
-	b.blocks = append(b.blocks, newBlock)
-}
-
-func (b blockChain) listBlocks(){
-	for _, block := range b.blocks{
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("Hash: %s\n", block.hash)
-		fmt.Printf("Prev Hash: %s\n", block.prevHash)
-	}
-}
-
 func main() {
-	chain := blockChain{}
-	chain.addBlock("first")
-	chain.addBlock("second")
-	chain.addBlock("third")
-	chain.listBlocks()
+	chain := blockchain.GetBlockChain()
+	fmt.Println(chain)
 }
