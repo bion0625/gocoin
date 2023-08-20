89d7545532a071d98e6e7563b35855a1ec669000
이의정 <stxtory@gmail.com>
2023-08-20 09:53:04 +0900

    open server

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/gocoin/blockchain"
+	"log"
+	"net/http"
 )
 
+var port string = ":4000"
+
+func home (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello from home!")
+}
+
 func main() {
-	chain := blockchain.GetBlockChain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Four Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
-	}
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
