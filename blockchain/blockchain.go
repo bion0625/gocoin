df9d12fc70e728616dd073c13774cfc524ab669b
이의정 <stxtory@gmail.com>
2023-08-26 16:38:50 +0900

    height & detail api

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Block struct {
-	Data     string
-	Hash     string
-	PrevHash string
+	Data     string	`json:"data"`
+	Hash     string	`json:"hash"`
+	PrevHash string	`json:"prehash,omitempty"`
+	Height	 int	`json:"height"`
 }
 
 type blockChain struct {
@@ -33,7 +34,7 @@ func getLastHash() string {
 }
 
 func createBlock(data string) *Block {
-	newBlock := Block{data, "", getLastHash()}
+	newBlock := Block{data, "", getLastHash(), len(GetBlockChain().blocks)+1}
 	newBlock.calculateHash()
 	return &newBlock
 }
@@ -54,4 +55,8 @@ func GetBlockChain() *blockChain {
 
 func (b *blockChain) AllBlocks() []*Block {
 	return b.blocks
+}
+
+func (b *blockChain) GetBlock(height int) *Block {
+	return b.blocks[height-1]
 }
